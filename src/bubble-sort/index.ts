d33719ded98eea5ae39464013f115631a74ca7c2
yanyunchangfeng <yanyunchangfeng@gmail.com>
2019-05-10 12:03:35 +0800

    add insert-sort

--- src/bubble-sort/index.ts
+++ src/bubble-sort/index.ts
@@ -4,7 +4,7 @@
    * @param  {[object Array]} arr    [要进行排序的数组]
    * 
    */
-const bubbleSort = (arr:any[])=>{
+const bubbleSort = (arr:any[]):void => {
    for(let i = 1; i<arr.length;i++){
        for(let j = 0;j<arr.length-i;j++){
            if(arr[j]>arr[j+1]){
