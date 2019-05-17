309e9f7e8c9245e5a4c83256317939136325a0ad
yanyunchangfeng <yanyunchangfeng@gmail.com>
2019-05-17 11:26:04 +0800

    update calendar-algorithm

--- algorithm/calendar-algorithm/index.js
+++ algorithm/calendar-algorithm/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var getMonthData = function (year, month) {
-    //用来保存最后的结果集
+    //定义ret变量用来保存最后的结果集
     var ret = [];
     if (!year || !month) {
         var date = new Date();
@@ -13,22 +13,25 @@ var getMonthData = function (year, month) {
     var preMonthDay = firstDayOfMonth.getDay();
     //获取上月的最后一天，本月第0天即为上月最后一天
     var lastDayOfLastMonth = new Date(year, month - 1, 0);
-    var lasteDateOflastMonth = lastDayOfLastMonth.getDate();
+    var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
     //获取本月最后一天，下月第0天即为本月最后一天
     var lastDayOfMonth = new Date(year, month, 0);
-    var lasteDateOfMonth = lastDayOfMonth.getDate();
+    var lastDateOfMonth = lastDayOfMonth.getDate();
     for (var i = 0; i < 6 * 7; i++) {
-        var thisYear = year, thisMonth = month, date = void 0, showDate = void 0;
-        date = i + 1 - preMonthDay;
-        showDate = date;
+        // 获取当前排序的日期数,+1是为了修正
+        var thisYear = year, thisMonth = month, date = i + 1 - preMonthDay, showDate = date;
         if (date <= 0) {
+            //如果date小于等于0，则说明是上月预留天数，月和日都要加以修正
             thisMonth -= 1;
-            showDate = date + lasteDateOflastMonth;
+            showDate = date + lastDateOfLastMonth;
         }
-        else if (date > lasteDateOfMonth) {
+        else if (date > lastDateOfMonth) {
+            //如果date大于本月最后一天说明为下月预留天数，月和日加以修正
             thisMonth += 1;
-            showDate = date - lasteDateOfMonth;
+            showDate = date - lastDateOfMonth;
         }
+        // 修正年月，因为当月份为1时，上月减1为0,需要同时修正月和年
+        // 当月份为12时，下月加1为13,需要同时修正年和月
         if (thisMonth === 13) {
             thisMonth = 1, thisYear += 1;
         }
@@ -36,8 +39,8 @@ var getMonthData = function (year, month) {
             thisMonth = 12, thisYear -= 1;
         }
         ret.push({
-            year: year,
-            month: month,
+            thisYear: thisYear,
+            thisMonth: thisMonth,
             date: date,
             showDate: showDate
         });
--- src/calendar-algorithm/index.ts
+++ src/calendar-algorithm/index.ts
@@ -11,11 +11,11 @@
      showDate:number,
  }
 
- const getMonthData = (year?:number,month?:number):DateRet[] => {
-     //用来保存最后的结果集
+ const getMonthData = (year?:number, month?:number):DateRet[] => {
+    //定义ret变量用来保存最后的结果集
     let ret:any[] = [];
-    if(!year||!month){
-        let date = new Date()
+    if( !year || !month){
+        let date = new Date();
         year = date.getFullYear();
         month = date.getMonth()+1; //月份修正
     }
@@ -24,28 +24,33 @@
     let preMonthDay =  firstDayOfMonth.getDay();
     //获取上月的最后一天，本月第0天即为上月最后一天
     let lastDayOfLastMonth = new Date(year, month-1, 0);
-    let lasteDateOflastMonth = lastDayOfLastMonth.getDate();
+    let lastDateOfLastMonth = lastDayOfLastMonth.getDate();
     //获取本月最后一天，下月第0天即为本月最后一天
     let lastDayOfMonth = new Date(year, month, 0);
-    let lasteDateOfMonth = lastDayOfMonth.getDate();
+    let lastDateOfMonth = lastDayOfMonth.getDate();
     
-    for(let i = 0;i < 6*7; i++){
-        let thisYear = year , thisMonth = month,date:number,showDate:number;
-        date = i+1-preMonthDay;
-        showDate = date;
-        if(date<=0){
+    for(let i = 0; i < 6 * 7; i++){
+        // 获取当前排序的日期数,+1是为了修正
+        let thisYear:number = year ,
+            thisMonth:number = month, 
+            date:number = i+1-preMonthDay,
+            showDate:number = date;
+        if(date <= 0){
+            //如果date小于等于0，则说明是上月预留天数，月和日都要加以修正
             thisMonth -= 1;
-            showDate = date + lasteDateOflastMonth
-        }else if(date>lasteDateOfMonth){
+            showDate = date + lastDateOfLastMonth;
+        }else if(date > lastDateOfMonth){
+            //如果date大于本月最后一天说明为下月预留天数，月和日加以修正
             thisMonth += 1;
-            showDate = date - lasteDateOfMonth
+            showDate = date - lastDateOfMonth;
         }
-        
+        // 修正年月，因为当月份为1时，上月减1为0,需要同时修正月和年
+        // 当月份为12时，下月加1为13,需要同时修正年和月
         if(thisMonth === 13) {thisMonth = 1 , thisYear += 1}
         if(thisMonth === 0) {thisMonth = 12 , thisYear -= 1}
         ret.push({
-            year,
-            month,
+            thisYear,
+            thisMonth,
             date,
             showDate
         })
